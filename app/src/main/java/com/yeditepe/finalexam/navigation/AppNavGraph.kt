0893d65fdb96dae336f3627016fc63cdb6292798
aybrkkya <133198008+aybrkkya@users.noreply.github.com>
2026-01-06 15:29:16 +0300

    Update AppNavGraph.kt

--- app/src/main/java/com/yeditepe/finalexam/navigation/AppNavGraph.kt
+++ app/src/main/java/com/yeditepe/finalexam/navigation/AppNavGraph.kt
@@ -1,8 +1,11 @@
 package com.yeditepe.finalexam.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.yeditepe.finalexam.ui.TaskDetailScreen
 
 @Composable
 fun AppNavGraph() {
@@ -15,7 +18,23 @@ fun AppNavGraph() {
     ) {
 
         // TODO 1: Add composable for "tasks"
+        composable("tasks") {
+            TasksScreen(navController = navController)
+        }
+
 
         // TODO 2: Add composable for "taskDetail/{title}"
+        composable("taskDetail/{title}") {
+            TaskDetailScreen(
+                title = it.arguments?.getString("title") ?: ""
+            )
+        }
+
+
     }
+}
+
+@Composable
+fun TasksScreen(navController: NavHostController) {
+
 }
