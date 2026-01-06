f6a7dc2c852a83d4c28e10305784a32f4e3d1199
aybrkkya <133198008+aybrkkya@users.noreply.github.com>
2026-01-06 15:31:05 +0300

    Update TaskRepository.kt

--- app/src/main/java/com/yeditepe/finalexam/repository/TaskRepository.kt
+++ app/src/main/java/com/yeditepe/finalexam/repository/TaskRepository.kt
@@ -7,7 +7,15 @@ class TaskRepository(private val api: TaskApi) {
 
     suspend fun fetchTasks(): List<Task> {
         // TODO 1: Call API
+        val dtoList = api.getTasks()
+
         // TODO 2: Convert TaskDto list to Task list
-        return TODO("Provide the return value")
+
+        return TODO("return dtoList.map { dto ->\n" +
+                "    Task(\n" +
+                "        title = dto.title,\n" +
+                "        isCompleted = dto.isCompleted\n" +
+                "    )\n" +
+                "}\n")
     }
 }
